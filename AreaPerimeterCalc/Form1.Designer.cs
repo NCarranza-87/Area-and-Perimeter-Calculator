3e62183ed05ba78b6f052c3409cea788c3adfb26
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-02 15:43:14 -0700

    Add project files.

---
+++ AreaPerimeterCalc/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace AreaPerimeterCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLength
+            // 
+            this.lblLength.Location = new System.Drawing.Point(28, 24);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(100, 23);
+            this.lblLength.TabIndex = 0;
+            this.lblLength.Text = "Length:";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.Location = new System.Drawing.Point(28, 48);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(100, 23);
+            this.lblWidth.TabIndex = 1;
+            this.lblWidth.Text = "Width:";
+            // 
+            // lblArea
+            // 
+            this.lblArea.Location = new System.Drawing.Point(28, 71);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(100, 23);
+            this.lblArea.TabIndex = 2;
+            this.lblArea.Text = "Area:";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.Location = new System.Drawing.Point(28, 96);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(100, 23);
+            this.lblPerimeter.TabIndex = 3;
+            this.lblPerimeter.Text = "Perimeter";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(31, 128);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(127, 127);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(85, 47);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 6;
+            // 
+            // txtArea
+            // 
+            this.txtArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArea.Location = new System.Drawing.Point(85, 71);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 7;
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPerimeter.Location = new System.Drawing.Point(85, 94);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.Size = new System.Drawing.Size(100, 20);
+            this.txtPerimeter.TabIndex = 8;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(85, 21);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 9;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(62, 156);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 23);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset Calculator";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 195);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Name = "Form1";
+            this.Text = "Area and Perimeter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ AreaPerimeterCalc/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaPerimeterCalc
+{
+    public partial class Form1 : Form
+    {
+
+        /******************************************
+         * This program will have a user to enter 
+         * a value for length and width. With the
+         * length and width entered from the user;
+         * the program will calculate the area 
+         * and perimeter.
+         *****************************************/
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /******************************************
+             * When the program loads, upon start up
+             * the txtArea and txtPerimeter will not
+             * allow a user to enter input. It will
+             * only display the values calculated from
+             * txtLength and txtWidth
+             ******************************************/
+
+            //  txtArea and txtPerimeter will
+            //  invoke method ReadOnly
+            txtArea.ReadOnly = true;
+            txtPerimeter.ReadOnly = true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /***********************************
+             * The Calculate button will get
+             * the input from the txtLength
+             * txtWidth and display the results
+             * in the txtArea and txt Perimeter
+             ***********************************/
+
+            // get the length from user length 
+            // variable will be named lengthVal
+            decimal lengthVal = Convert.ToDecimal(txtLength.Text);
+
+            
+            if (lengthVal < 0)
+            {                   // validate for negative input
+                MessageBox.Show("please enter a positive number!");
+                return;
+            }
+
+            // get the width from user width
+            // variable will be name widthVal
+            decimal widthVal = Convert.ToDecimal(txtWidth.Text);
+
+            
+            if(widthVal < 0)
+            {                   // validate for negative input
+                MessageBox.Show("please enter a positive number!");
+                return;
+            }
+
+            //calculate lengthVal and widthVal to get the area
+            //and set as a result variable AreaResult
+            double AreaResult = Convert.ToDouble(widthVal * lengthVal);
+
+            //calculate lengthVal and widthVal to get the area
+            //and set as a result variable PerimeterResult
+            double PerimeterResult = Convert.ToDouble((2 * lengthVal) + (2 * widthVal));
+
+            //display the AreaResult
+            //formatted to 2 decimal places
+            txtArea.Text = AreaResult.ToString("f2");
+
+            //display the PerimeterResult
+            //formateed to 2 decimal places
+            txtPerimeter.Text = PerimeterResult.ToString("f2");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            /*
+             * when clicked the Reset button will
+             * remove all values from txtLength,
+             * txtWidth, txtPerimeter and txtArea;
+             * then begin with the input cursor
+             * set back to txtLength for input
+             */
+
+            //all textboxes are cleared
+            txtLength.Text = "";
+            txtWidth.Text = "";
+            txtPerimeter.Text = "";
+            txtArea.Text = "";
+
+            //txtLength invokes
+            //Focus method to reenter
+            //input from user
+            txtLength.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            /*********************************
+             * When Exit button is click
+             * the user is done with entering
+             * length and width input
+             *********************************/
+
+            // end program and close form
+            this.Close();
+        }
+    }
+}
